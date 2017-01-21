c560629dfc81188f4fbbbb3f131c22f0b55f58a5
Eduardo Nicolas Quintana <linkstudioarg@gmail.com>
2017-01-21 11:29:37 -0300

    New Props, crouch and no jumping

---
+++ Assets/Scripts/ReportVertices.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReportVertices : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        
+        Debug.LogWarning(gameObject.name + " vertices: " + mesh.vertexCount/3);
+    }
+	
+}
